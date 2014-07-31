370a546ea0c501cdf663b47f09fb07d949a88d8d
Valerio Vaudi <valerio.vaudi@gmail.com>
2014-07-31 08:34:58 +0200

    test

---
+++ src/test/java/it/valeriovaudi/findmodel/AbstractGenericTestConfiguration.java
@@ -0,0 +1,10 @@
+package it.valeriovaudi.findmodel;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Valerio on 31/07/2014.
+ */
+public abstract class AbstractGenericTestConfiguration {
+    protected static Logger logger = Logger.getLogger(AbstractGenericTestConfiguration.class);
+}
--- src/test/java/it/valeriovaudi/findmodel/ParseFormulaTest.java
+++ src/test/java/it/valeriovaudi/findmodel/ParseFormulaTest.java
@@ -2,29 +2,47 @@ package it.valeriovaudi.findmodel;
 
 import it.valeriovadui.findmodel.control.OperatorControl;
 import it.valeriovadui.findmodel.factory.ParserFactory;
+import it.valeriovadui.findmodel.model.ElementaryFormula;
+import it.valeriovadui.findmodel.model.Expression;
 import it.valeriovadui.findmodel.model.Formula;
 import it.valeriovadui.findmodel.service.Parser;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.mockito.Mockito.when;
+
 /**
  * Created by Valerio on 30/07/2014.
  */
-public class ParseFormulaTest {
+public class ParseFormulaTest extends AbstractGenericTestConfiguration{
 
+    @MockitoAnnotations.Mock
+    Parser parser;
 
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    /*
+    * Test that the evaluation of formula: ( a => b ) is correct
+    * For this i'm mock the parser interface and test only the evaluation operation without test the parsing work
+    * */
     @Test
     public void parseFormulaTest(){
-        String formulaString = "( a => b )";
+        Formula resultFormulaFirstElement = new ElementaryFormula("a",false);
+        Formula resultFormulaSecondElement = new ElementaryFormula("b",false);
 
-        Parser parser = ParserFactory.getCompelxParser(formulaString);
+        Formula resultFormulaExpression = new Expression(resultFormulaFirstElement,resultFormulaSecondElement,Expression.IMPLICATION,false);
+        when(parser.parse()).thenReturn(resultFormulaExpression);
 
         Formula formula = parser.parse();
         String[] allVariable = formula.getAllVariable();
         List<Map<String, Boolean>> hashMaps = OperatorControl.checkModel(formula, allVariable);
-        System.out.println(hashMaps);
-
+        logger.info(hashMaps);
     }
 }
